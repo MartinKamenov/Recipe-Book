5b6431b2212d0347d71e0f9ea49e5b481aca8133
MartinKamenov <martinkamenov10@gmail.com>
2020-07-25 20:34:14 +0300

    Add recipe list component

---
+++ src/components/recipe/list/RecipeList.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import RecipeDetails from '../details';
+
+const RecipeList = ({recipes}) => {
+  // Using href as unique identifier, so I filter recipes with same href
+  const filteredRecipes = recipes.reduce((acc, curr) => acc.find((r) => r.href === curr.href) ? acc : [...acc, curr], []);
+  return (
+    <div className='container'>
+      <div className='recipe-list-container row'>
+        {filteredRecipes.map((recipe) => <RecipeDetails recipe={recipe} key={recipe.href}/>)}
+      </div>
+    </div>
+  );
+};
+
+RecipeList.propTypes = {
+  recipes: PropTypes.arrayOf({
+    title: PropTypes.string.isRequired,
+    href: PropTypes.string.isRequired,
+    ingredients: PropTypes.string.isRequired,
+    thumbnail: PropTypes.string,
+  }).isRequired,
+};
+
+export default RecipeList;
